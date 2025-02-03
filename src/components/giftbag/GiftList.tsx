a6bac959cf6ca5ad0c1c6cc4e2c131807b5a5a20
xxxjinn <tlsguswls319@naver.com>
2025-02-04 02:03:17 +0900

    design: shadcn tooltip (기본) 추가

--- src/components/giftbag/GiftList.tsx
+++ src/components/giftbag/GiftList.tsx
@@ -1,25 +1,50 @@
 import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const GiftList = () => {
   const images = ["/img/gift_blank_square.svg", "/img/gift_blank_round.svg"];
 
   return (
-    <div className="grid grid-cols-2 h-[396px] grid-rows-[repeat(6,_1fr)]">
-      {Array.from({ length: 6 }).map((_, index) => (
-        <div
-          key={index}
-          className="w-[130px] h-[130px] p-[10px] flex justify-center items-center"
-        >
-          <Image
-            src={images[index % 2]}
-            alt={`gift-item-${index}`}
-            className="w-full h-full object-contain cursor-pointer hover:opacity-[75%]"
-            width="110"
-            height="110"
-          />
-        </div>
-      ))}
-    </div>
+    <TooltipProvider>
+      <div className="grid grid-cols-2 h-[396px] grid-rows-[repeat(6,_1fr)]">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <div
+            key={index}
+            className="w-[130px] h-[130px] p-[10px] flex justify-center items-center"
+          >
+            {index === 0 ? (
+              <Tooltip>
+                <TooltipTrigger>
+                  <Image
+                    src={images[index % 2]}
+                    alt={`gift-item-${index}`}
+                    className="w-full h-full object-contain cursor-pointer hover:opacity-[75%]"
+                    width="110"
+                    height="110"
+                  />
+                </TooltipTrigger>
+                <TooltipContent side="top" align="center">
+                  사진으로 간단하게 <br /> 선물박스를 채워볼까요?
+                </TooltipContent>
+              </Tooltip>
+            ) : (
+              <Image
+                src={images[index % 2]}
+                alt={`gift-item-${index}`}
+                className="w-full h-full object-contain cursor-pointer hover:opacity-[75%]"
+                width="110"
+                height="110"
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </TooltipProvider>
   );
 };
 
