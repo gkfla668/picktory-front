c85a816a0029012fc3e731f613b4d2eae744606a
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-02-03 20:33:41 +0900

    fix: card width 줄어듬 방지

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,14 +10,17 @@ interface CardProps {
 }
 
 const Card = ({ img, size, isActive, onClick }: CardProps) => {
-  const sizeClasses = size === "small" ? "w-[70px] h-[70px]" : "w-22 h-22";
+  const sizeClasses =
+    size === "small"
+      ? "w-[70px] h-[70px] min-w-[70px]"
+      : "w-[88px] h-[88px] min-w-[88px]";
   const borderColorClasses = isActive ? "border-gray-700" : "border-gray-100";
 
   const imageSize = size === "small" ? 60 : 75;
 
   return (
     <div
-      className={`flex justify-center border-[1.4px] items-center ${borderColorClasses} ${sizeClasses} rounded-xl box-border bg-gray-5 p-1`}
+      className={`flex justify-center border-[1.4px] items-center ${borderColorClasses} ${sizeClasses} rounded-xl box-border bg-gray-50 p-1`}
       onClick={onClick}
     >
       <Image
