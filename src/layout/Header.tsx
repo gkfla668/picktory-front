f5c8c466a2635a2aabb4577240c41ca718283e54
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-02-04 15:53:51 +0900

    fix: header 렌더링 로직 수정 및 url 매핑

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -11,9 +11,8 @@ import ArrowLeftIcon from "../../public/icons/arrow_left_large.svg";
 // 정적 title 관리
 // 임시 매핑
 const pageTitles: { [key: string]: string } = {
-  "/make-gift": "선물 보따리 만들기",
-  "/fill-gift": "선물 박스 채우기",
-  "/send-gift": "선물 보따리 배달하기",
+  "/giftbag": "선물 보따리 만들기",
+  "/gift-upload": "선물 박스 채우기",
   "/setting": "설정",
   "/my-bottari": "내가 만든 보따리",
 };
@@ -34,10 +33,21 @@ const Header = () => {
 
   useEffect(() => {
     const title = searchParams?.get("title"); // 쿼리 파라미터에서 title 가져오기
+
     if (title) {
-      setDynamicTitle(title);
+      setDynamicTitle(title); // title이 있으면 그걸로 설정
+    } else {
+      // pageTitles의 모든 경로에 대해 pathname이 포함되는지 확인
+      const matchedTitle = Object.keys(pageTitles).find(
+        (key) => pathname && pathname.includes(key),
+      );
+      if (matchedTitle) {
+        setDynamicTitle(pageTitles[matchedTitle]);
+      } else {
+        setDynamicTitle("PICKTORY"); // 기본 제목 설정
+      }
     }
-  }, [searchParams]);
+  }, [pathname, searchParams]);
 
   // 메인 페이지: 로고 + 설정 아이콘
   if (isHomePage) {
