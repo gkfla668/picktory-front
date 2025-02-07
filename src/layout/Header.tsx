b2cebb13d37ea2cc2f32a9a68786c74d4faf8f96
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-02-08 00:08:16 +0900

    feat: 헤더 url 매핑

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -12,10 +12,10 @@ import { useEditBoxStore } from "@/stores/gift-upload/useStore";
 // 정적 title 관리
 // 임시 매핑
 const pageTitles: { [key: string]: string } = {
+  "/giftbag/list": "내가 만든 보따리",
   "/giftbag": "선물 보따리 만들기",
   "/gift-upload": "선물 박스 채우기",
   "/setting": "설정",
-  "/my-bottari": "내가 만든 보따리",
 };
 
 const Header = () => {
