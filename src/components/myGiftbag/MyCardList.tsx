423bbd0e0fc4ea0caf37abffdd416c3466eb437f
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-02-18 13:44:44 +0900

    feat: myCardList 컴포넌트 img 값 추가

--- src/components/myGiftbag/MyCardList.tsx
+++ src/components/myGiftbag/MyCardList.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 import Card from "@/components/common/Card";
 
 import { FilledGiftListPreview, MyGiftBagPreview } from "@/types/giftbag/types";
 
+import { DESIGN_TYPE_MAP } from "@/constants/constants";
+
 interface MyCardListProps {
   type?: "design" | "image";
-  data: MyGiftBagPreview[] | FilledGiftListPreview[] | string[];
+  data: MyGiftBagPreview[] | FilledGiftListPreview[];
   size: "small" | "medium";
   giftbagIndex?: string;
 }
@@ -25,20 +27,34 @@ const MyCardList = ({ type, data, size, giftbagIndex }: MyCardListProps) => {
     }
   };
 
+  const hasThumbnailField = (
+    item: MyGiftBagPreview | FilledGiftListPreview,
+  ): item is FilledGiftListPreview => {
+    return "thumbnail" in item;
+  };
+
   return (
     <div className="flex gap-[12px] whitespace-nowrap">
       {data &&
-        Array.from({ length: data.length }, (_, index) => (
-          <Card
-            key={index}
-            type={type}
-            size={size}
-            img={""}
-            onClick={() => handleCardClick(index)}
-          />
-        ))}
+        data.map((item, index) => {
+          const giftBagDesignURL =
+            "designType" in item && DESIGN_TYPE_MAP[item.designType];
+
+          return (
+            <Card
+              key={index}
+              type={type}
+              size={size}
+              img={
+                hasThumbnailField(item)
+                  ? item.thumbnail
+                  : giftBagDesignURL || ""
+              }
+              onClick={() => handleCardClick(index)}
+            />
+          );
+        })}
     </div>
   );
 };
-
 export default React.memo(MyCardList);
