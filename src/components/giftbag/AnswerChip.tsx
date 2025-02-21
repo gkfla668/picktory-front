6fb3b046e8bc3e694fb3158a95906235e34b3400
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-02-21 16:34:38 +0900

    design: 보따리 결과 answer chip font size 수정

--- src/components/giftbag/AnswerChip.tsx
+++ src/components/giftbag/AnswerChip.tsx
@@ -4,9 +4,7 @@ interface AnswerChipProps {
 
 const AnswerChip = ({ text }: AnswerChipProps) => {
   return (
-    <span className="rounded-md bg-gray-100 py-2 px-3 h-[39px] w-auto text-xs">
-      {text}
-    </span>
+    <span className="rounded-md bg-gray-100 py-2 px-3 text-[15px]">{text}</span>
   );
 };
 
