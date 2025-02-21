4415381a0ca75106064ebef4c51342facce955fd
xxxjinn <tlsguswls319@naver.com>
2025-02-22 03:48:45 +0900

    refactor: 원래대로 돌려놓기,,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import Header from "@/layout/Header";
 import { Toaster } from "@/components/ui/toaster";
 import PageTransition from "@/app/PageTransition";
 import { Providers } from "./providers";
-import VhFix from "./vhFix";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
@@ -56,7 +55,12 @@ export default function RootLayout({
             <PageTransition>
               <Header />
               <Providers>
-                <VhFix bgColor={bgColor}>{children}</VhFix>
+                <div
+                  className={`flex-grow ${bgColor}`}
+                  style={{ height: "calc(100vh - 56px)" }}
+                >
+                  {children}
+                </div>
                 <Toaster />
               </Providers>
             </PageTransition>
