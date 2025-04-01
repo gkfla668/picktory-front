32d599cdd4a6fff1e57974c312bd77f8eb8bf47b
임하림 <gkfla668@naver.com>
2025-04-01 12:01:34 +0900

    refactor: 메인 이미지가 아닌 "보따리 만들러 가기" 버튼만 눌렀을 때 이동하도록 수정 (#124)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,12 +5,13 @@ import Image from "next/image";
 
 import MyCardList from "@/components/myGiftbag/MyCardList";
 import Loading from "@/components/common/Loading";
+import { Icon } from "@/components/common/Icon";
+import { Button } from "@/components/ui/button";
 import { useGiftBagPreview } from "@/hooks/api/useMyGiftBagPreview";
 import useResetStore from "@/hooks/useResetStore";
 
 import MainGraphic from "/public/img/main_graphic.svg";
 import ArrowRightIcon from "/public/icons/arrow_right_small.svg";
-import { Icon } from "@/components/common/Icon";
 
 const Page = () => {
   useResetStore();
@@ -20,16 +21,21 @@ const Page = () => {
 
   return (
     <main className="flex flex-col gap-10 items-center justify-center pt-3 px-4">
-      <Link href="/giftbag/select" className="mx-[2px]">
+      <div className="relative">
         <Image
           src={MainGraphic}
           alt="MainGraphic"
           width={394}
           height={346}
           loading="eager"
-          className="hover:opacity-70"
         />
-      </Link>
+        <Link
+          href="/giftbag/select"
+          className="absolute bottom-3 left-1/2 -translate-x-1/2 w-[calc(100%-24px)] max-w-[370px]"
+        >
+          <Button size="lg">보따리 만들러 가기</Button>
+        </Link>
+      </div>
       <section className="flex flex-col gap-[14px] w-full">
         <div className="flex justify-between items-center">
           <p className="font-medium text-gray-900">내가 만든 보따리</p>
