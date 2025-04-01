4994964eb662d31ba081c15fa99fd72920508368
임하림 <gkfla668@naver.com>
2025-04-01 12:01:43 +0900

    design: 카카오톡으로 시작하기 버튼 hover 시 투명도 제거 (#125)

--- src/components/common/KakaoLoginButton.tsx
+++ src/components/common/KakaoLoginButton.tsx
@@ -5,7 +5,7 @@ const KakaoLoginButton = ({ link }: { link: string }) => {
   return (
     <a
       href={link}
-      className="bg-[#FEE500] flex py-[17px] justify-center rounded-sm px-4 hover:opacity-90"
+      className="bg-[#FEE500] flex py-[17px] justify-center rounded-sm px-4"
     >
       <Icon src={LogoKakaoIcon} alt="logo" className="absolute left-[34px]" />
       <p className="text-sm font-medium">카카오톡으로 시작하기</p>
