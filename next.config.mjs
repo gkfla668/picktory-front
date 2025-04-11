2824c2f5676f52f1ad13f3d5e097f4e8df36b08b
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-04-11 15:31:04 +0900

    refactor: api url 환경 변수 처리

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: "https://api.picktory.net/api/v1/:path*",
+        destination: `${process.env.API_URL}/api/v1/:path*`,
       },
     ];
   },
